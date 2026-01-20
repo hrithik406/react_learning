15a4a77454f943e811ac9eb8928714432bd301fe
Hrithik Raj <hrithikraj6400@gmail.com>
2026-01-20 19:59:40 +0530

    dog api card by destructuring

--- src/app/dashboard/components/NavBtn.tsx
+++ src/app/dashboard/components/NavBtn.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 import { NavlistType } from "../layout"
 
 
-export default function NavBtn(navBtn:NavlistType) {
+export default function NavBtn(navBtn: NavlistType) {
     return (
         <Link rel="stylesheet" href={navBtn.linked}>
             <button className={`w-full flex items-center space-x-3 px-4 py-2 text-sm xl:text-base text-gray-600 hover:bg-gray-100 rounded-lg hover:font-medium hover:text-black hover:px-6 transition-all duration-300 hover:cursor-pointer focus:bg-gray-200 focus:text-black focus:font-medium focus:shadow-[5px_2px_10px_rgba(0,0,0,0.1)`}>
@@ -12,3 +12,4 @@ export default function NavBtn(navBtn:NavlistType) {
         </Link>
     )
 }
+    
--- src/app/dashboard/dog/page.tsx
+++ src/app/dashboard/dog/page.tsx
@@ -27,8 +27,36 @@ type BreedType = {
         }
     }
 }
+const DogApiCard = ((breed: BreedType) => {
+    const href = `/dashboard/dog/${breed}`
+    return (
+        <div key={breed.id} className="bg-white rounded-xl p-6 md:p-8 border border-gray-300  hover:shadow-lg transition-shadow" >
+            <h2 className="flex text-black text-xl border-b-0"><strong>{breed.attributes.name}</strong></h2>
+
+            <div className="text-black text-lg">
+                Lifespan: {breed.attributes.life.min} - {breed.attributes.life.max} years
+            </div>
+            <div className="flex flex-col text-lg text-black">
+                Weight:
+                <span>Male: {breed.attributes.male_weight.min} - {breed.attributes.male_weight.max} years</span>
+                <span>Female: {breed.attributes.female_weight.min} - {breed.attributes.female_weight.max} years</span>
+            </div>
+
+            <p className="text-black text-sm line-clamp-3">
+                {breed.attributes.description}
+            </p>
+
+            <div className="text-black">
+                <strong>Hypoallergenic:</strong> {breed.attributes.hypoallergenic ? "✅ Yes" : "❌ No"}
+            </div>
+        </div>
+    )
+}
+)
+
+
 export default async function DogBreedsPage() {
-    const response = await fetch('https://dogapi.dog/api/v2/breeds')
+    const response = await fetch('https://dogapi.dog/api/v2/breeds?page%5Bnumber%5D=1&page%5Bsize%5D=12')
 
     const json = await response.json();
     const breeds: BreedType[] = json.data;
@@ -40,27 +68,8 @@ export default async function DogBreedsPage() {
             <h1 className="flex mx-auto font-bold text-center text-black text-6xl mb-8">Dog Breeds</h1>
 
             <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-6">
-                {breeds.map((breed) => (
-                    <div key={breed.id} className="bg-white rounded-xl p-6 md:p-8 border border-gray-300  hover:shadow-lg transition-shadow" >
-                        <h2 className="flex text-black text-xl border-b-0"><strong>{breed.attributes.name}</strong></h2>
-
-                        <div className="text-black text-lg">
-                            Lifespan: {breed.attributes.life.min} - {breed.attributes.life.max} years
-                        </div>
-                        <div className="flex flex-col text-lg text-black">
-                            Weight:
-                            <span>Male: {breed.attributes.male_weight.min} - {breed.attributes.male_weight.max} years</span>
-                            <span>Female: {breed.attributes.female_weight.min} - {breed.attributes.female_weight.max} years</span>
-                        </div>
-
-                        <p className="text-black text-sm line-clamp-3">
-                            {breed.attributes.description}
-                        </p>
-
-                        <div className="text-black">
-                            <strong>Hypoallergenic:</strong> {breed.attributes.hypoallergenic ? "✅ Yes" : "❌ No"}
-                        </div>
-                    </div>
+                {breeds.map((breed, idx) => (
+                    <DogApiCard key={idx} {...breed} />
                 ))}
             </div>
         </main>
